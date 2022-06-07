3e11e7a20c8b628a119f514af609274bd5fd17d6
Kaixi Hou <kaixih@nvidia.com>
2022-06-07 15:26:34 -0700

    change default conv options

--- utils.h
+++ utils.h
@@ -86,14 +86,14 @@ void ComputeStrides(MatMulOpts& opts) {
 
 std::optional<ConvOpts> ParseConvOpts(int argc, char** argv) {
   struct CmdConvOpts {
-    std::string input_dims = "3,4,5,5";
-    std::string filter_dims = "4,4,2,2,2";
+    std::string input_dims = "3,8,10,10";
+    std::string filter_dims = "8,8,2,2,1";
     std::string bias_dims = "0,0,0,0,0";
     std::string paddings = "1,1,1";
     std::string strides = "1,1,1";
     std::string dilations = "1,1,1";
-    int data_format = 0;
-    int data_type = 0;
+    int data_format = 1;
+    int data_type = 1;
     int conv_kind = 0;
     int leakyrelu_kind = 0;
   };
