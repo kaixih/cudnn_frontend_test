b247866620d4efb9a304f953727b1ffbddd1459a
kaixih <kaixih@nvidia.com>
2022-06-08 10:58:17 -0700

    update scripts

--- sanity_check_matmul.sh
+++ sanity_check_matmul.sh
@@ -9,21 +9,21 @@ function ProcessLog() {
 
 # Varying M
 for (( i = 1 ; i <= 20 ; i++ )); do
-  ./test_matmul_bias_tanh.out --input0 1,$i,8 --input1 1,8,8 --bias 1,1,8 &> log.tmp
+  ./test_matmul_bias_activation.out --input0 1,$i,8 --input1 1,8,8 --bias 1,1,8 &> log.tmp
   echo -n "M=$i K=8 N=8 "
   ProcessLog log.tmp
 done
 
 # Varying K
 for (( i = 1 ; i <= 20 ; i++ )); do
-  ./test_matmul_bias_tanh.out --input0 1,8,$i --input1 1,$i,8 --bias 1,1,8 &> log.tmp
+  ./test_matmul_bias_activation.out --input0 1,8,$i --input1 1,$i,8 --bias 1,1,8 &> log.tmp
   echo -n "M=8 K=$i N=8 "
   ProcessLog log.tmp
 done
 
 # Varying N
 for (( i = 1 ; i <= 20 ; i++ )); do
-  ./test_matmul_bias_tanh.out --input0 1,8,8 --input1 1,8,$i --bias 1,1,$i &> log.tmp
+  ./test_matmul_bias_activation.out --input0 1,8,8 --input1 1,8,$i --bias 1,1,$i &> log.tmp
   echo -n "M=8 K=8 N=$i "
   ProcessLog log.tmp
 done
